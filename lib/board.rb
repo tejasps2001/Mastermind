4aafb2d96d5138a8291296c6039d20e9695ad224
Tejas P S <tejasps2001@gmail.com>
2026-01-14 09:48:44 +0530

    Fix the printing of the board
    The previous code was printing the entire 2D array in every line.
    Instead, only one array of the 2D array should be printed in a line.

--- lib/board.rb
+++ lib/board.rb
@@ -2,18 +2,24 @@
 
 # Class to represent an instance of the board
 class Board
+  attr_accessor :secret_code, :key_holes, :curr_guess
+  attr_reader :code_holes
+
   def initialize(total_guesses)
-    @code_holes = Array.new(12) {Array.new(4, '-')}
+    @code_holes = Array.new(12) { Array.new(4, '-') }
     @key_holes = Array.new(12) { Array.new(4, ' ') }
     @total_guesses = total_guesses
+    # curr_guess will be updated when the breaker passes a guess
+    @curr_guess = 0
+    @secret_code = Array.new(4, 0)
   end
 
   def print_board
     puts '----------------------------------------------------------\n'
     12.times do |i|
       print '|'
-      print key_hole[i]
-      str.join(code_hole, ' ')
+      print str.join(@key_holes[i], ' ')
+      print str.join(@code_holes[i], ' ')
       print '|\n\n'
     end
     puts '----------------------------------------------------------'
